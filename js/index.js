063b486891c28af7a9a21b6091b4350c0fb20778
leaclaire <leah.kiplagat@student.moringaschool.com>
2021-04-20 00:09:33 +0300

    updating js

--- js/index.js
+++ js/index.js
@@ -30,5 +30,47 @@ $(document).ready(function () {
       .mouseout(function(){
         $("#project1").hide();
       })
+      $(".project2").mouseover(function(){
+        $("#project2").show();
+      })
+      .mouseout(function(){
+        $("#project2").hide();
+      })
+      $(".project3").mouseover(function(){
+        $("#project3").show();
+      })
+      .mouseout(function(){
+        $("#project3").hide();
+      })
+      $(".project4").mouseover(function(){
+        $("#project4").show();
+      })
+      .mouseout(function(){
+        $("#project4").hide();
+      })
+      $(".project5").mouseover(function(){
+        $("#project5").show();
+      })
+      .mouseout(function(){
+        $("#project5").hide();
+      })
+      $(".project6").mouseover(function(){
+        $("#project6").show();
+      })
+      .mouseout(function(){
+        $("#project6").hide();
+      })
+      $(".project7").mouseover(function(){
+        $("#project7").show();
+      })
+      .mouseout(function(){
+        $("#project7").hide();
+      })
+      $(".project8").mouseover(function(){
+        $("#project8").show();
+      })
+      .mouseout(function(){
+        $("#project8").hide();
+      })
   });
    
